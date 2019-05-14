7cc2b680a478658d3177b0a6944a73a0628f991a
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-14 15:25:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+min=Float.new
+name_hash.collect do |name,value|
+  if value < min
+    min = value
+  end
+end
 end
